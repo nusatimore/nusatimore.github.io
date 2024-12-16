c13d8ea5434808cd3df1c9dbd03e9ec9909fce15
Nusa Timore <nusatimoretelematika@gmail.com>
2024-12-16 19:30:35 +0800

    clearing tab and styles transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+const navDocmBtn = document.querySelector("#nav-docm-btn");
+const navDocm = document.querySelector("#nav-docm");
+
+navDocmBtn.addEventListener("click", function () {
+  navDocm.classList.toggle("ntt-nav-docm-open");
+});
 // scrolly
 const item = document.getElementById("gear");
 window.addEventListener("scroll", () => {
---
+++ src/js/scripts.js
@@ -0,0 +1,1 @@
+// all script is write by it dept. of nusa timore telematika
